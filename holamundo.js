686c45e7c61d56910b361e12d7b5292a45a70046
Pablo Sulbaran <psulbaran@cenditel.gob.ve>
2019-02-06 08:52:30 -0400

    Ejemplos básicos de Javascript

---
+++ holamundo.js
@@ -0,0 +1,4 @@
+<SCRIPT LANGUAGE="JavaScript" type="text/javascript">
+    console.log("Hola mundo");
+    document.write("Hola mundo...");
+</SCRIPT>
---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hola mundo");
+// concatenación
+console.log("hasta " + "luego");
+// operaciones matemáticas
+console.log(2.8 + 3);
+//variables introducidas por el usuario
+edad = prompt("Introduzca la edad");
+console.log("La edad introducida es " + edad);
